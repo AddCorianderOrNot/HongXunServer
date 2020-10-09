8acf40295a777847807f5e9fa4502c86e61a332a
L-M-Sherlock <l.m.kagamigawachloe@gmail.com>
2020-10-09 10:37:18 +0800

    重命名message控制器，避免与将来要开发的模块混淆。

---
+++ controllers/message_controller.go
@@ -0,0 +1,43 @@
+package controllers
+
+import (
+	"HongXunServer/models"
+	"HongXunServer/services"
+	"github.com/kataras/iris/v12"
+	"log"
+)
+
+type MessageController struct {
+	Ctx     iris.Context
+	Service services.MessageService
+}
+
+func (c *MessageController) Get() {
+	m, err := c.Service.LoadMessage()
+	if err != nil {
+		c.Ctx.NotFound()
+		return
+	}
+	_, err = c.Ctx.JSON(m)
+
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func (c *MessageController) Post() {
+	var message models.Message
+	err := c.Ctx.ReadJSON(&message)
+	if err != nil {
+		log.Println(err)
+		c.Ctx.StopWithProblem(iris.StatusBadRequest, iris.NewProblem())
+		return
+	}
+	err = c.Service.SaveMessage(message)
+	if err != nil {
+		log.Println(err)
+		c.Ctx.StopWithProblem(iris.StatusBadRequest, iris.NewProblem())
+		return
+	}
+	c.Ctx.JSON(iris.Map{"is_success": true})
+}
