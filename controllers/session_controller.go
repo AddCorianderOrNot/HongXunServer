2de993658f2851870e276b05c40d4e979a2eea88
L-M-Sherlock <l.m.kagamigawachloe@gmail.com>
2020-10-09 23:42:16 +0800

    添加用户登入模块

---
+++ controllers/session_controller.go
@@ -0,0 +1,43 @@
+package controllers
+
+import (
+	"HongXunServer/models"
+	"HongXunServer/services"
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/middleware/jwt"
+	"log"
+	"time"
+)
+
+type SessionController struct {
+	Ctx     iris.Context
+	Service services.UserService
+}
+
+type UserClaims struct {
+	jwt.Claims
+	email string
+}
+
+func (c *SessionController) Post() {
+	var authentication models.Authentication
+	err := c.Ctx.ReadJSON(&authentication)
+	if err != nil {
+		log.Println(err)
+	}
+	isVerified := c.Service.Verify(authentication)
+	if isVerified {
+		j := jwt.HMAC(15*time.Minute, "secret", "itsa16bytesecret")
+		claims := UserClaims{
+			Claims: j.Expiry(jwt.Claims{
+				Issuer:   "an-issuer",
+				Audience: jwt.Audience{"an-audience"},
+			}),
+			email: authentication.Email,
+		}
+		accessToken, _ := j.Token(claims)
+		c.Ctx.JSON(iris.Map{"is_success": true, "token": accessToken})
+	} else {
+		c.Ctx.JSON(iris.Map{"is_success": false, "token": nil})
+	}
+}
--- main.go
+++ main.go
@@ -5,27 +5,30 @@ import (
 	"HongXunServer/services"
 	"context"
 	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/middleware/jwt"
 	"github.com/kataras/iris/v12/mvc"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
+	"time"
 )
 
 func main() {
+
+	app := iris.New()
+	app.Logger().SetLevel("debug")
+
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 	client, err := mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	err = client.Ping(context.Background(), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer client.Disconnect(context.TODO())
-
 	db := client.Database("hungxun")
-
 	var (
 		messagesCollection = db.Collection("messages")
 		messageService     = services.NewMessageService(messagesCollection)
@@ -33,16 +36,18 @@ func main() {
 		userService        = services.NewUserService(userCollection)
 	)
 
-	app := iris.New()
-	app.Logger().SetLevel("debug")
-
 	message := mvc.New(app.Party("/message"))
 	message.Register(messageService)
+	message.Router.Use(jwt.HMAC(15*time.Minute, "secret", "itsa16bytesecret").Verify)
 	message.Handle(new(controllers.MessageController))
 
 	user := mvc.New(app.Party("/user"))
 	user.Register(userService)
 	user.Handle(new(controllers.UserController))
+
+	session := mvc.New(app.Party("/session"))
+	session.Register(userService)
+	session.Handle(new(controllers.SessionController))
 
 	app.Run(
 		// Start the web server at localhost:8080
