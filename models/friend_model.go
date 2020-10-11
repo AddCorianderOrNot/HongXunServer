b79935b629ebf5de5bb3e63a84bf4820566b8da0
L-M-Sherlock <l.m.kagamigawachloe@gmail.com>
2020-10-11 16:53:17 +0800

    添加好友关系模型

---
+++ models/friend_model.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type Friend struct {
+	Id         primitive.ObjectID `json:"_id" bson:"_id"`
+	OwnerId    primitive.ObjectID `json:"owner_id" bson:"owner_id"`
+	FriendId   primitive.ObjectID `json:"friend_id" bson:"friend_id"`
+	Remarks    string             `json:"remarks" bson:"remarks"`
+	CreateTime time.Time          `json:"create_time" bson:"create_time"`
+}
