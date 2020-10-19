53afe20cb86d39f5633642d3e72791d04ca5c105
L-M-Sherlock <l.m.kagamigawachloe@gmail.com>
2020-10-19 21:21:08 +0800

    修复数据库存储冲突

--- models/message_model.go
+++ models/message_model.go
@@ -9,7 +9,7 @@ type Message struct {
 	Id         primitive.ObjectID `json:"_id" bson:"_id"`
 	CreateTime time.Time          `json:"dateTime" bson:"create_time"`
 	UserFrom   string 			  `json:"sender" bson:"user_from"`
-	UserName   string 			  `json:"senderName" bson:"user_from"`
+	UserName   string 			  `json:"senderName" bson:"UserName"`
 	UserTo     string             `json:"receiver" bson:"user_to"`
 	Type       int                `json:"messageType" bson:"type"`
 	Content    string             `json:"message" bson:"content"`
